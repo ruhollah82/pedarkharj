187657d4d2a5da5c98c4d54b9c104ba65f6ee52a
Rhuhollah <0robena0@gmail.com>
2025-04-03 15:14:48 +0330

    removed junk import and comments from app.tsx and corrected claculator directory name

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import "./App.css";
 import theme from "./theme/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import Layout from "./layouts/Layout/Layout";
@@ -7,26 +5,17 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./modules/Home/Home";
 import Contacts from "./modules/Contacts/Contacts";
 import Account from "./modules/Account/Account";
-import Calculator from "./modules/Claculator/Calculator";
+import Calculator from "./modules/Calculator/Calculator";
 import Search from "./modules/Search/Search";
 import { useAuth } from "./context/AuthContext";
 import Authentication from "./modules/Authentication/Authentication";
-import { CacheProvider } from "@emotion/react";
-import createCache from "@emotion/cache";
-import rtlPlugin from "stylis-plugin-rtl";
-import { prefixer } from "stylis";
 
-const cacheRtl = createCache({
-  key: "mui-rtl",
-  stylisPlugins: [prefixer, rtlPlugin],
-});
 function App() {
   const { isAuthenticated } = useAuth(); // Access the authentication status from context
   // var isAuthenticated = true;
   return (
-    // <CacheProvider value={cacheRtl}>
     <ThemeProvider theme={theme}>
-      {true ? (
+      {isAuthenticated ? (
         <Layout>
           <Routes>
             <Route path="/app/home" element={<Home />} />
@@ -45,7 +34,6 @@ function App() {
         </Routes>
       )}
     </ThemeProvider>
-    // </CacheProvider>
   );
 }
 
